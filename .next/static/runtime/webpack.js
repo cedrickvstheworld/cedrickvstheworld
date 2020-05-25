5c8007919d8ebba499eceb3658b1f0be6fee167d
cedie712 <cedrick048@yahoo.com>
2020-05-25 16:53:46 +0800

    update some small thing

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "19d2aa80bfb4e568df1a";
+/******/ 	var hotCurrentHash = "8c668cbf21f510fbf825";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
