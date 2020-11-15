bd4fe0cbd3ecb3ab896ab80a1b14cbe95be9df75
cedrickvstheworld <38357125+cedrickvstheworld@users.noreply.github.com>
2020-11-15 18:13:14 +0800

    hotfix - update facebook profile link

--- contents/links.js
+++ contents/links.js
@@ -1,4 +1,4 @@
 export const github_profile = 'https://github.com/cedrickvstheworld';
-export const facebook_profile = 'https://web.facebook.com/cedrick.domingo.75';
+export const facebook_profile = 'https://web.facebook.com/cedrickvstheworld';
 export const skype_profile = 'https://join.skype.com/invite/XTHFKLhTqVeo';
 export const linked_in = 'https://www.linkedin.com/in/cedrickvstheworld';
